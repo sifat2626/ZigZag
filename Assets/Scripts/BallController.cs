c61754fe9c16e72a757ecfe52f5f2cc694e55a5c
sifat2626 <ashrafulsifat26@gmail.com>
2021-11-30 23:12:42 +0600

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed;
+    private bool started;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!started)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                started = true;
+                rb.velocity = new Vector3(0,0,speed);
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            SwitchDirection();
+        }
+    }
+
+    void SwitchDirection()
+    {
+        if (rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(speed,0,0);
+        }
+        else if (rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector3(0,0,speed);
+        }
+    }
+}
